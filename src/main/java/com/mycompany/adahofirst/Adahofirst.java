800ada3b94a92440a9bdf11d3d6d49d07eac1c98
26104317 <26104317@mvula.univen.ac.za>
2026-05-15 16:39:07 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/adahofirst/Adahofirst.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adahofirst;
+
+/**
+ *
+ * @author Mudau Adaho
+ */
+public class Adahofirst {
+
+    public static void main(String[] args) {
+        System.out.println("I am Adaho mudau");
+    }
+}
